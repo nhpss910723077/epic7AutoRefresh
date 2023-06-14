8d9766845a6f2490a5e9915a8664c95b7df00531
nhpss910723077 <nhpss91064@gmail.com>
2023-06-14 16:14:23 +0800

    add some information

--- test.py
+++ test.py
@@ -16,6 +16,9 @@ maxTime = int(input("輸入最大次數: "))
 print("按下 F1 結束程式");
 keyboard.on_press(ExitControl)
 
+# 開始時間
+start_time = time.time()
+
 # 連接夜神模擬器
 # simulator = u2.connect('127.0.0.1:62001')
 simulator = u2.connect()
@@ -61,8 +64,19 @@ while exitControl and totalTime <= maxTime:
     
     totalTime += 1
     
+# 結束時間
+end_time = time.time()
+# 計算經過的時間（秒）
+elapsed_time = end_time - start_time
+# 將經過的時間轉換為分鐘和秒數
+minutes = int(elapsed_time // 60)  # 取整數部分為分鐘數
+seconds = int(elapsed_time % 60)  # 取餘數部分為秒數
+    
+# 輸出經過的時間
+print("耗時：", minutes, "分", seconds, "秒")
 print("已刷新次數 : " + str(totalTime))
 print("已花費天空石 : " + str(totalTime * 3))
+print("已花費金幣 : " + str(totalCovenant * 184000 + totalMystic * 280000))
 print("已取得聖約書籤 : " + str(totalCovenant * 5))
 print("已取得神秘書籤 : " + str(totalMystic * 50))
 print("聖約書籤機率 : " + str((totalCovenant / totalTime) * 100) + "%")
